70db8f5d1d13f0fbdbdb4f21e600833394e36509
tweppy <tweppy@gmail.com>
2023-04-27 18:31:00 +0200

    add 500 error catch

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -9,44 +9,65 @@ const { findUserByID } = require("../models/userModel");
 
 async function get(req, res) {
   const { userID } = req.body;
-  const data = await getAllNotes(userID);
-  res.status(200).json(data);
+
+  try {
+    const data = await getAllNotes(userID);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function add(req, res) {
   const data = req.body;
-  const user = await findUserByID(req.id);
-  await addNote(data, user);
 
-  const result = {
-    message: `${data.title} added to notes.`,
-    username: user.username,
-    userID: user.userID,
-    note: data,
-  };
+  try {
+    const user = await findUserByID(req.id);
+    await addNote(data, user);
+    const result = {
+      message: `${data.title} added to notes.`,
+      username: user.username,
+      userID: user.userID,
+      note: data,
+    };
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function edit(req, res) {
   const data = req.body;
-  await editNote(data);
 
-  res.status(200).json({ message: `Note '${data.id}' has been edited.` });
+  try {
+    await editNote(data);
+    res.status(200).json({ message: `Note '${data.id}' has been edited.` });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function remove(req, res) {
   const data = req.body;
-  await removeNote(data);
 
-  res.status(200).json({ message: `Note with id '${data.id}' deleted.` });
+  try {
+    await removeNote(data);
+    res.status(200).json({ message: `Note with id '${data.id}' deleted.` });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function search(req, res) {
   const { title } = req.body;
-  const result = await findNoteByTitle(title);
 
-  res.status(200).json({ result: result });
+  try {
+    const result = await findNoteByTitle(title);
+    res.status(200).json({ result: result });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 module.exports = { add, get, edit, remove, search };
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,37 +2,49 @@ const { addUser, findUserByID } = require("../models/userModel");
 
 async function userSignup(req, res) {
   const credentials = req.body;
-  await addUser(credentials);
 
-  const result = {
-    success: true,
-    message: `User ${credentials.username} successfully signed up!`,
-  };
+  try {
+    await addUser(credentials);
+    const result = {
+      success: true,
+      message: `User ${credentials.username} successfully signed up!`,
+    };
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function userLogin(req, res) {
   const { username } = req.body;
 
-  const result = {
-    success: true,
-    message: `${username} successfully logged in.`,
-  };
+  try {
+    const result = {
+      success: true,
+      message: `${username} successfully logged in.`,
+    };
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 async function userAccount(req, res) {
   const user = await findUserByID(req.id);
 
-  const result = {
-    success: true,
-    username: user.username,
-    userID: user.userID,
-  };
+  try {
+    const result = {
+      success: true,
+      username: user.username,
+      userID: user.userID,
+    };
 
-  res.status(200).json(result);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
 }
 
 module.exports = { userSignup, userLogin, userAccount };
